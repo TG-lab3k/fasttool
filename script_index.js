d3c3d0ecca81b933689e541646437a278c06132a
NjLaolei <leigt3@outlook.com>
2024-05-13 00:15:32 +0800

    add convert function

--- script_index.js
+++ script_index.js
@@ -1,3 +1,5 @@
+const FORMAT_DEFAULT = 'YYYY-MM-DD HH:mm:ss';
+
 function convertTimestampToGMT(timestamp, offset) {
     var date = new Date(timestamp);
     var localOffset = date.getTimezoneOffset();
@@ -25,42 +27,83 @@ function getCurrentUtcOffset() {
     return utcOffset
 }
 
+function convertMillisToGMT(millis, format, offset) {
+    var momentObj = moment.unix(millis);
+    var formattedTime = momentObj.utcOffset(offset).format(format);
+    return formattedTime;
+}
+
+function convertGMTToMillis(datetime, format) {
+    var momentObj = moment(datetime, format);
+    //momentObj.utcOffset(2);
+    var millis = momentObj.unix();
+    return millis;
+}
+
+function clickConvertMillisToGMT() {
+    var timestampInput = document.querySelector('.timestamp-datetime-text');
+    var formatInput = document.querySelector('.timestamp-datetime-format');
+    var gmtInput = document.querySelector('.timestamp-datetime-timezones');
+    var timestampText = String(timestampInput.value);
+    if (timestampText.trim() === '') {
+        console.log('timestampText is null');
+        return;
+    }
+
+
+    var timestamp = Number(timestampText);
+    var formatText = String(formatInput.value);
+    if (formatText.trim() === '') {
+        formatText = FORMAT_DEFAULT;
+    }
+
+    var gmtText = String(gmtInput.value);
+    var utcOffset = 0;
+    if (gmtText.trim() !== '') {
+        console.log(gmtText);
+        utcOffset = parseGMTOffset(gmtText);
+    }
+
+    if (utcOffset === 0 || utcOffset === NaN) {
+        utcOffset = getCurrentUtcOffset();
+    }
+    console.log('utcOffset', utcOffset);
+    var formattedTime = convertMillisToGMT(timestamp, formatText, utcOffset);
+    var result = document.querySelector('.timestamp-datetime-result');
+    result.innerHTML = formattedTime;
+}
+
+function clickConvertGMTToMillis() {
+    var datetimeInput = document.querySelector('.datetime-timestamp-text');
+    var datetimeText = String(datetimeInput.value);
+    if (datetimeText.trim() === '') {
+        return;
+    }
+
+    var formatInput = document.querySelector('.datetime-timestamp-format');
+    var formatText = String(formatInput.value);
+    if (formatText.trim() === '') {
+        formatText = FORMAT_DEFAULT;
+    }
+
+    var millis = convertGMTToMillis(datetimeText, formatText);
+    var resultDiv = document.querySelector('.datetime-timestamp-result');
+    resultDiv.innerHTML = millis;
+}
+
+function bindingEvent(componentName, eventName, action) {
+    var component = document.querySelector(componentName);
+    component.addEventListener(eventName, action);
+}
+
+function app() {
+    bindingEvent('.timestamp-datetime-convert', 'click', clickConvertMillisToGMT);
+    bindingEvent('.datetime-timestamp-convert', 'click', clickConvertGMTToMillis);
+}
+
 function boost() {
-    var timestampDateTimebutton = document.querySelector('.timestamp-datetime-convert');
-    timestampDateTimebutton.addEventListener('click', function () {
-        var timestampInput = document.querySelector('.timestamp-datetime-text');
-        var formatInput = document.querySelector('.timestamp-datetime-format');
-        var gmtInput = document.querySelector('.timestamp-datetime-timezones');
-        var timestampText = String(timestampInput.value);
-        if (timestampText.trim() === '') {
-            console.log('timestampText is null');
-            return;
-        }
-
-
-        var timestamp = Number(timestampText);
-        var formatText = String(formatInput.value);
-        if (formatText.trim() === '') {
-            formatText = 'YYYY-MM-DD HH:mm:ss';
-        }
-
-        var gmtText = String(gmtInput.value);
-        var utcOffset = 0;
-        if (gmtText.trim() !== '') {
-            console.log(gmtText);
-            utcOffset = parseGMTOffset(gmtText);
-        }
-
-        if(utcOffset === 0){
-            utcOffset = getCurrentUtcOffset();
-        }
-        console.log('utcOffset', utcOffset);
-
-        var momentObj = moment.unix(timestamp);
-        var formattedTime = momentObj.utcOffset(utcOffset).format(formatText);
-        var result = document.querySelector('.timestamp-datetime-result');
-        result.innerHTML = formattedTime;
-    });
-
-    console.log('boost loaded!!');
+    window.onload = function () {
+        app();
+    }
 };
+boost();
